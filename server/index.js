5eaa21ec8b95f0d623a13de63602ab39e4aa4103
Debatreyta <debatreyadas@gmail.com>
2024-01-19 17:49:37 +0530

    Again backwards

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const { config } = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const { config } = require('dotenv');
 config();
-const cloneRoutes = require('./routes/clone.routes.js')
+const cloneRoutes = require('./routes/clone.routes.js');
 
 const app = express();
 
 // MIDDLEWARE
 app.use(express.json());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL]
+    origin: "http://localhost:5173"
 }));
 
 app.use('/api', cloneRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
     console.log('App is running');
-})
+});
